3f59645e529bd1a6a018f7783a1705cacd653daf
Skye <134712151+afshyna@users.noreply.github.com>
2026-06-15 18:13:32 +0200

    Update failover.sh

--- vpn-openvpn-wireguard-engineering-project/scripts/failover.sh
+++ vpn-openvpn-wireguard-engineering-project/scripts/failover.sh
@@ -8,7 +8,7 @@ ping -c 1 -W 1 $TARGET_IP > /dev/null
 
 if [ $? -eq 0 ]; then
     # Backup tunnel is UP -> Route traffic directly via local tun0 (VPN backup server used)
-	ip route replace 172.20.10.0/28 via 10.9.2.2 dev tun0
+	ip route replace 172.20.10.0/28 via 10.9.2.1 dev tun0
 else
     # Backup tunnel is DOWN -> Route traffic via Paris LAN gateway (primary route)
 	ip route replace 172.20.10.0/28 via 192.168.100.200 dev enp0s8
