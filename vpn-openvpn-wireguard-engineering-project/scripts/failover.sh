870c908f485827343afd597c22c815a583b6fc3c
Skye <134712151+afshyna@users.noreply.github.com>
2026-06-15 08:29:07 +0200

    Update failover.sh

--- vpn-openvpn-wireguard-engineering-project/scripts/failover.sh
+++ vpn-openvpn-wireguard-engineering-project/scripts/failover.sh
@@ -1,13 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
-# Test de reachabilite : Test VPN de secours (IP tunnel cote Tokyo)
-ping -c 1 -W 1 10.9.2.2 > /dev/null
+# Target: Backup VPN tunnel endpoint IP on Tokyo's side
+TARGET_IP="10.9.2.2"
+
+# Reachability test
+ping -c 1 -W 1 $TARGET_IP > /dev/null
 
 if [ $? -eq 0 ]; then
 	# Tunnel VPN de secours est UP : route vers le serveur VPN de secours utilisé
+    # Backup tunnel is UP -> Route traffic directly via local tun0 (VPN backup server used)
 	ip route replace 172.20.10.0/28 via 10.9.2.2 dev tun0
 else
-	# Tunnel VPN est DOWN : route principale utilisé
+    # Backup tunnel is DOWN -> Route traffic via Paris LAN gateway (primary route)
 	ip route replace 172.20.10.0/28 via 192.168.100.200 dev enp0s8
-
 fi
