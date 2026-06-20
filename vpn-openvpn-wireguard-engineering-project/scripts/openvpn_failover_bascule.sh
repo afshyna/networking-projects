82007760856792b1b432a4c32062fb17c020929a
Skye <134712151+afshyna@users.noreply.github.com>
2026-06-20 17:04:09 +0200

    Update openvpn_failover_bascule.sh

--- vpn-openvpn-wireguard-engineering-project/scripts/openvpn_failover_bascule.sh
+++ vpn-openvpn-wireguard-engineering-project/scripts/openvpn_failover_bascule.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Target: Backup VPN tunnel endpoint IP on Tokyo's side
-TARGET_IP="10.9.2.2"
+# Reachability test of the Primary VPN (Paris) 
+ping -c 1 -W 1 10.9.1.1 > /dev/null
 
-# Reachability test
-ping -c 1 -W 1 $TARGET_IP > /dev/null
+# VPN Backup (Auber) DOWN (<=> VPN principal UP): stop the backup vpn (if active)
+if [ $? -eq 0 ]; then			
+	netstat -lu | grep 1195 > /dev/null
+	if [ $? -eq 0 ];then		# $? = 0 => srv-auber actif, $?1 => srv auber pas actif
+		sudo systemctl stop openvpn@srv-auber
+	fi
 
-if [ $? -eq 0 ]; then
-    # Backup tunnel is UP -> Route traffic directly via local tun0 (VPN backup server used)
-	ip route replace 172.20.10.0/28 via 10.9.2.1 dev tun0
+# VPN Primary (Paris) DOWN → launch the Backup VPN if not already active 
 else
-    # Backup tunnel is DOWN -> Route traffic via Paris LAN gateway (primary route)
-	ip route replace 172.20.10.0/28 via 192.168.100.200 dev enp0s8
+	netstat -lu | grep 1195 > /dev/null
+	if [ $? -eq 1 ]; then 		
+		sudo systemctl start openvpn@srv-auber
+	fi
 fi
